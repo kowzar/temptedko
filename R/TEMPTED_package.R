9a54824784ac52a4907d57a24197f596a66d1ab4
owzar001 <owzar001@duke.edu>
2024-01-08 16:00:04 -0500

    first step to fixing vignette build

--- R/TEMPTED_package.R
+++ R/TEMPTED_package.R
@@ -1,7 +1,12 @@
+#' TEMPTED
+#' @name TEMPTED
+#' @docType package
+#' @description Package R file
 #' @importFrom stats  quantile lm qnorm reshape
 #' @importFrom np  npreg
 #' @importFrom ggplot2  ggplot geom_line geom_ribbon facet_wrap aes ylab
 #' @importFrom methods  is
+#' @importFrom dplyr filter
+#' @importFrom gridExtra grid.arrange
 NULL
-#> NULL
 
